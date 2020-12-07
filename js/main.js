93ffc8e3965439ab0d8248c38b4e680b8df3060e
Jean Paul Matos <mat9807jp@gmail.com>
2020-12-07 15:50:10 -0500

    Añadimos el menú responsive a la página

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 /* --------------- HEADER EFFECT --------------- */
-window.addEventListener('scroll', function () {
+/* window.addEventListener('scroll', function () {
 
     let mainHeader = document.getElementById('main__header');
     let mainSocialHeader1 = document.getElementById('main__social__header1');
@@ -19,5 +19,61 @@ window.addEventListener('scroll', function () {
 
     }
 
-});
+}); */
 
+
+
+// PROPIEDADES MENU MOVIL
+
+var propMenu = {
+
+    burger_menu: document.getElementById('burger_menu'),
+    slideMenu: document.getElementById('slideMenu'),
+    menu_activo: false,
+    elem_menu: document.querySelectorAll('#slideMenu .menu-principal-link')
+
+}
+
+
+// METODOS MENU MOVIL
+
+var metMenu = {
+
+    inicio: function () {
+
+        propMenu.burger_menu.addEventListener('click', metMenu.toggleMenu);
+
+        for(var i = 0; i < propMenu.elem_menu.length; i++) {
+
+            propMenu.elem_menu[i].addEventListener('click', metMenu.ocultarMenu);
+
+        }
+
+    },
+
+    toggleMenu: function () {
+
+        if (propMenu.menu_activo == false) {
+
+            propMenu.menu_activo = true;
+            propMenu.slideMenu.className = propMenu.slideMenu.className + ' active';
+
+        } else {
+
+            propMenu.menu_activo = false;
+            propMenu.slideMenu.className = propMenu.slideMenu.className.replace('active', '');
+
+        }
+
+    },
+
+    ocultarMenu: function () {
+
+        propMenu.menu_activo = false;
+        propMenu.slideMenu.className = propMenu.slideMenu.className.replace('active', '');
+
+    }
+
+}
+
+metMenu.inicio();
