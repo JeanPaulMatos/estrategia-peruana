e9e5d0d2c67f1ccba251861a24cb7888a8dd2388
jeanpaulmatos <mat9807jp@gmail.com>
2021-04-01 11:22:59 -0500

    Subimos nuevos cambios al Diseño Responsive

--- js/landing/slider.js
+++ js/landing/slider.js
@@ -198,6 +198,612 @@ window.addEventListener('load', function () {
                     itemWidth: 450,
                 }
             }
+            , 
+            
+            {
+                breakpoint: 413,
+                settings: {
+                    draggable: true,
+                    slidesToShow: '1.25',
+                    // slidesToScroll: 'auto',
+                    itemWidth: 450,
+                }
+            }
+            , 
+            
+            {
+                breakpoint: 412,
+                settings: {
+                    draggable: true,
+                    slidesToShow: '1.25',
+                    // slidesToScroll: 'auto',
+                    itemWidth: 450,
+                }
+            }
+            , 
+            
+            {
+                breakpoint: 411,
+                settings: {
+                    draggable: true,
+                    slidesToShow: '1.25',
+                    // slidesToScroll: 'auto',
+                    itemWidth: 450,
+                }
+            }
+            , 
+            
+            {
+                breakpoint: 410,
+                settings: {
+                    draggable: true,
+                    slidesToShow: '1.25',
+                    // slidesToScroll: 'auto',
+                    itemWidth: 450,
+                }
+            }
+            , 
+            
+            {
+                breakpoint: 409,
+                settings: {
+                    draggable: true,
+                    slidesToShow: '1.25',
+                    // slidesToScroll: 'auto',
+                    itemWidth: 450,
+                }
+            }
+            , 
+            
+            {
+                breakpoint: 408,
+                settings: {
+                    draggable: true,
+                    slidesToShow: '1.25',
+                    // slidesToScroll: 'auto',
+                    itemWidth: 450,
+                }
+            }
+            , 
+            
+            {
+                breakpoint: 407,
+                settings: {
+                    draggable: true,
+                    slidesToShow: '1.25',
+                    // slidesToScroll: 'auto',
+                    itemWidth: 450,
+                }
+            }
+            , 
+            
+            {
+                breakpoint: 406,
+                settings: {
+                    draggable: true,
+                    slidesToShow: '1.25',
+                    // slidesToScroll: 'auto',
+                    itemWidth: 450,
+                }
+            }
+            , 
+            
+            {
+                breakpoint: 405,
+                settings: {
+                    draggable: true,
+                    slidesToShow: '1.25',
+                    // slidesToScroll: 'auto',
+                    itemWidth: 450,
+                }
+            }
+            , 
+            
+            {
+                breakpoint: 404,
+                settings: {
+                    draggable: true,
+                    slidesToShow: '1.25',
+                    // slidesToScroll: 'auto',
+                    itemWidth: 450,
+                }
+            }
+            , 
+            
+            {
+                breakpoint: 403,
+                settings: {
+                    draggable: true,
+                    slidesToShow: '1.25',
+                    // slidesToScroll: 'auto',
+                    itemWidth: 450,
+                }
+            }
+            , 
+            
+            {
+                breakpoint: 402,
+                settings: {
+                    draggable: true,
+                    slidesToShow: '1.25',
+                    // slidesToScroll: 'auto',
+                    itemWidth: 450,
+                }
+            }
+            , 
+            
+            {
+                breakpoint: 401,
+                settings: {
+                    draggable: true,
+                    slidesToShow: '1.25',
+                    // slidesToScroll: 'auto',
+                    itemWidth: 450,
+                }
+            }
+            , 
+            
+            {
+                breakpoint: 400,
+                settings: {
+                    draggable: true,
+                    slidesToShow: '1.25',
+                    // slidesToScroll: 'auto',
+                    itemWidth: 450,
+                }
+            }
+            , 
+            
+            {
+                breakpoint: 399,
+                settings: {
+                    draggable: true,
+                    slidesToShow: '1.25',
+                    // slidesToScroll: 'auto',
+                    itemWidth: 450,
+                }
+            }
+            , 
+            
+            {
+                breakpoint: 398,
+                settings: {
+                    draggable: true,
+                    slidesToShow: '1.25',
+                    // slidesToScroll: 'auto',
+                    itemWidth: 450,
+                }
+            }
+            , 
+            
+            {
+                breakpoint: 397,
+                settings: {
+                    draggable: true,
+                    slidesToShow: '1.25',
+                    // slidesToScroll: 'auto',
+                    itemWidth: 450,
+                }
+            }
+            , 
+            
+            {
+                breakpoint: 396,
+                settings: {
+                    draggable: true,
+                    slidesToShow: '1.25',
+                    // slidesToScroll: 'auto',
+                    itemWidth: 450,
+                }
+            }
+            , 
+            
+            {
+                breakpoint: 395,
+                settings: {
+                    draggable: true,
+                    slidesToShow: '1.25',
+                    // slidesToScroll: 'auto',
+                    itemWidth: 450,
+                }
+            }
+            , 
+            
+            {
+                breakpoint: 394,
+                settings: {
+                    draggable: true,
+                    slidesToShow: '1.25',
+                    // slidesToScroll: 'auto',
+                    itemWidth: 450,
+                }
+            }
+            , 
+            
+            {
+                breakpoint: 393,
+                settings: {
+                    draggable: true,
+                    slidesToShow: '1.25',
+                    // slidesToScroll: 'auto',
+                    itemWidth: 450,
+                }
+            }
+            , 
+            
+            {
+                breakpoint: 392,
+                settings: {
+                    draggable: true,
+                    slidesToShow: '1.25',
+                    // slidesToScroll: 'auto',
+                    itemWidth: 450,
+                }
+            }
+            , 
+            
+            {
+                breakpoint: 391,
+                settings: {
+                    draggable: true,
+                    slidesToShow: '1.25',
+                    // slidesToScroll: 'auto',
+                    itemWidth: 450,
+                }
+            }
+            , 
+            
+            {
+                breakpoint: 390,
+                settings: {
+                    draggable: true,
+                    slidesToShow: '1.25',
+                    // slidesToScroll: 'auto',
+                    itemWidth: 450,
+                }
+            }
+            , 
+            
+            {
+                breakpoint: 389,
+                settings: {
+                    draggable: true,
+                    slidesToShow: '1.25',
+                    // slidesToScroll: 'auto',
+                    itemWidth: 450,
+                }
+            }
+            , 
+            
+            {
+                breakpoint: 388,
+                settings: {
+                    draggable: true,
+                    slidesToShow: '1.25',
+                    // slidesToScroll: 'auto',
+                    itemWidth: 450,
+                }
+            }
+            , 
+            
+            {
+                breakpoint: 387,
+                settings: {
+                    draggable: true,
+                    slidesToShow: '1.25',
+                    // slidesToScroll: 'auto',
+                    itemWidth: 450,
+                }
+            }
+            , 
+            
+            {
+                breakpoint: 386,
+                settings: {
+                    draggable: true,
+                    slidesToShow: '1.25',
+                    // slidesToScroll: 'auto',
+                    itemWidth: 450,
+                }
+            }
+            , 
+            
+            {
+                breakpoint: 385,
+                settings: {
+                    draggable: true,
+                    slidesToShow: '1.25',
+                    // slidesToScroll: 'auto',
+                    itemWidth: 450,
+                }
+            }
+            , 
+            
+            {
+                breakpoint: 384,
+                settings: {
+                    draggable: true,
+                    slidesToShow: '1.25',
+                    // slidesToScroll: 'auto',
+                    itemWidth: 450,
+                }
+            }
+            , 
+            
+            {
+                breakpoint: 383,
+                settings: {
+                    draggable: true,
+                    slidesToShow: '1.25',
+                    // slidesToScroll: 'auto',
+                    itemWidth: 450,
+                }
+            }
+            , 
+            
+            {
+                breakpoint: 382,
+                settings: {
+                    draggable: true,
+                    slidesToShow: '1.25',
+                    // slidesToScroll: 'auto',
+                    itemWidth: 450,
+                }
+            }
+            , 
+            
+            {
+                breakpoint: 381,
+                settings: {
+                    draggable: true,
+                    slidesToShow: '1.25',
+                    // slidesToScroll: 'auto',
+                    itemWidth: 450,
+                }
+            }
+            , 
+            
+            {
+                breakpoint: 380,
+                settings: {
+                    draggable: true,
+                    slidesToShow: '1.25',
+                    // slidesToScroll: 'auto',
+                    itemWidth: 450,
+                }
+            }
+            , 
+            
+            {
+                breakpoint: 379,
+                settings: {
+                    draggable: true,
+                    slidesToShow: '1.25',
+                    // slidesToScroll: 'auto',
+                    itemWidth: 450,
+                }
+            }
+            , 
+            
+            {
+                breakpoint: 378,
+                settings: {
+                    draggable: true,
+                    slidesToShow: '1.25',
+                    // slidesToScroll: 'auto',
+                    itemWidth: 450,
+                }
+            }
+            , 
+            
+            {
+                breakpoint: 377,
+                settings: {
+                    draggable: true,
+                    slidesToShow: '1.25',
+                    // slidesToScroll: 'auto',
+                    itemWidth: 450,
+                }
+            }
+            , 
+            
+            {
+                breakpoint: 376,
+                settings: {
+                    draggable: true,
+                    slidesToShow: '1.25',
+                    // slidesToScroll: 'auto',
+                    itemWidth: 450,
+                }
+            }
+            , 
+            
+            {
+                breakpoint: 375,
+                settings: {
+                    // draggable: true,
+                    slidesToShow: '1.15',
+                    // slidesToScroll: 'auto',
+                    itemWidth: 450,
+                }
+            }
+            , 
+            
+            {
+                breakpoint: 374,
+                settings: {
+                    draggable: true,
+                    slidesToShow: '1.15',
+                    // slidesToScroll: 'auto',
+                    itemWidth: 450,
+                }
+            }
+            , 
+            
+            {
+                breakpoint: 373,
+                settings: {
+                    draggable: true,
+                    slidesToShow: '1.15',
+                    // slidesToScroll: 'auto',
+                    itemWidth: 450,
+                }
+            }
+            , 
+            
+            {
+                breakpoint: 372,
+                settings: {
+                    draggable: true,
+                    slidesToShow: '1.15',
+                    // slidesToScroll: 'auto',
+                    itemWidth: 450,
+                }
+            }
+            , 
+            
+            {
+                breakpoint: 371,
+                settings: {
+                    draggable: true,
+                    slidesToShow: '1.15',
+                    // slidesToScroll: 'auto',
+                    itemWidth: 450,
+                }
+            }
+            , 
+            
+            {
+                breakpoint: 370,
+                settings: {
+                    draggable: true,
+                    slidesToShow: '1.15',
+                    // slidesToScroll: 'auto',
+                    itemWidth: 450,
+                }
+            }
+            , 
+            
+            {
+                breakpoint: 369,
+                settings: {
+                    draggable: true,
+                    slidesToShow: '1.15',
+                    // slidesToScroll: 'auto',
+                    itemWidth: 450,
+                }
+            }
+            , 
+            
+            {
+                breakpoint: 368,
+                settings: {
+                    draggable: true,
+                    slidesToShow: '1.15',
+                    // slidesToScroll: 'auto',
+                    itemWidth: 450,
+                }
+            }
+            , 
+            
+            {
+                breakpoint: 367,
+                settings: {
+                    draggable: true,
+                    slidesToShow: '1.15',
+                    // slidesToScroll: 'auto',
+                    itemWidth: 450,
+                }
+            }
+            , 
+            
+            {
+                breakpoint: 366,
+                settings: {
+                    draggable: true,
+                    slidesToShow: '1.15',
+                    // slidesToScroll: 'auto',
+                    itemWidth: 450,
+                }
+            }
+            , 
+            
+            {
+                breakpoint: 365,
+                settings: {
+                    draggable: true,
+                    slidesToShow: '1.15',
+                    // slidesToScroll: 'auto',
+                    itemWidth: 450,
+                }
+            }
+            , 
+            
+            {
+                breakpoint: 400,
+                settings: {
+                    draggable: true,
+                    slidesToShow: '1.15',
+                    // slidesToScroll: 'auto',
+                    itemWidth: 450,
+                }
+            }
+            , 
+            
+            {
+                breakpoint: 364,
+                settings: {
+                    draggable: true,
+                    slidesToShow: '1.15',
+                    // slidesToScroll: 'auto',
+                    itemWidth: 450,
+                }
+            }
+            , 
+            
+            {
+                breakpoint: 363,
+                settings: {
+                    draggable: true,
+                    slidesToShow: '1.15',
+                    // slidesToScroll: 'auto',
+                    itemWidth: 450,
+                }
+            }
+            , 
+            
+            {
+                breakpoint: 362,
+                settings: {
+                    draggable: true,
+                    slidesToShow: '1.15',
+                    // slidesToScroll: 'auto',
+                    itemWidth: 450,
+                }
+            }
+            , 
+            
+            {
+                breakpoint: 361,
+                settings: {
+                    draggable: true,
+                    slidesToShow: '1.15',
+                    // slidesToScroll: 'auto',
+                    itemWidth: 450,
+                }
+            }
+            , 
+            
+            {
+                breakpoint: 360,
+                settings: {
+                    draggable: true,
+                    slidesToShow: '1.1',
+                    // slidesToScroll: 'auto',
+                    itemWidth: 450,
+                }
+            }
+            
 
         ]
 
